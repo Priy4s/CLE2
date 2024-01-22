56993bc3d455ed3db601e8f83b91536638d9f5f1
HRJustin <1087405@hr.nl>
2024-01-22 10:31:35 +0100

    De Front-End wat veranderd.

--- admin_page/capacity_edit.php
+++ admin_page/capacity_edit.php
@@ -55,13 +55,14 @@ mysqli_close($db);
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-    <title>Day capacity</title>
+    <title>ADMIN CAPACITY EDIT</title>
 </head>
 <body>
+<section class="section is-large">
     <form action="" method="post">
         <!--Field for capacity-->
         <div class="field">
-            <label class="label" for="name">Capaciteit</label>
+            <label class="label is-medium" for="name">Capaciteit</label>
             <div class="control">
                 <input class="input" id="name" placeholder="capacity" type="text" name="capacity" value=" <?= htmlentities($dayCapacity['capacity']) ?>">
                 <span class="help is-danger">
@@ -70,12 +71,13 @@ mysqli_close($db);
             </div>
         </div>
 
-        <div class="field is-centered">
+        <div class="field">
             <div class="field-label is-normal"></div>
             <div class="field-body">
-                <button class="button is-primary is-fullwidth" type="submit" name="submit">Save</button>
+                <button class="button is-primary is-medium" type="submit" name="submit">Save</button>
             </div>
         </div>
     </form>
+</section>
 </body>
 </html>
